d6f9488ed1fc468e7856e7a1536b28618eb0440f
madraju <madraju@lin83006613.corp.capgemini.com>
2022-01-19 13:38:24 +0530

    first commit

---
+++ ApiGateway/src/main/java/Config/SpringCloudConfig.java
@@ -0,0 +1,13 @@
+package Config;
+
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+//@Autowired
+//private JwtAuthenticationFilter filter;
+
+}
